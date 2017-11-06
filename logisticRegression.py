59cae69823971c9bac524c1dc9caec22189687d3
Greg Climer <greg.climer@multiply.com>
2017-11-06 15:12:43 -0600

    Fixed a bug in syntax and added some comments. Performing Logistic Regression correctly.

--- logisticRegression.py
+++ logisticRegression.py
@@ -1,10 +1,13 @@
 from math import exp
 
+# Use weights and input data to make a guess for pass or fail
 def makeGuess(xValue, values):
     guess = values[0]
-    guess += xValue + values[1]
-    return 1.0 / (1.0 + exp(-guess))
+    guess += xValue * values[1]
+    guess = 1.0 / (1.0 + exp(-guess))
+    return guess
 
+# Initialize data, weights, epochs, and learning rate
 rate = 0.1
 epochs = 1000
 values = [0, 1, 0, 1, 0, 1, 1, 1]
@@ -13,17 +16,19 @@ prevSqrErrors = 0
 
 print('training data to be evaluated:')
 for x in range(len(values)):
-    print(x + 1, values[x])
+    print('  ', x + 1, values[x])
 
 for epoch in range(epochs):
     sqrErrors = 0
-    for x in values:
+    for x in range(len(values)):
         guess = makeGuess(x + 1, result)
+        # Determine innacuracy of our guess
         error = values[x] - guess
         sqrErrors += error**2
+        # Update weights
         result[0] += rate * error * guess * (1.0 - guess)
-        result[1] += rate * error * guess * (1.0 - guess) * (x  + 1)
-    # print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, rate, sqrErrors))
+        result[1] += rate * error * guess * (1.0 - guess) * (x + 1)
+    # Check if there is an improvement on the guesses since last iteration
     if sqrErrors == prevSqrErrors:
         print('Optimal found, discontinuing iteration after epoch %d' % epoch)
         break
@@ -31,6 +36,5 @@ for epoch in range(epochs):
 print('final weights:', result)
 for x in range(len(values)):
     guess = makeGuess(x + 1, result)
-    print('x value=%d, y=%d, prediction=%3f' % (x + 1, values[x], guess))
-
+    print('   x value=%d, y=%d, prediction=%3f' % (x + 1, values[x], guess))
 
